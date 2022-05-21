94811a8c14ef01baf18e378cff49a99c1c21379b
juhahaha <22dldydgml@naver.com>
2022-05-21 22:28:49 +0900

    2주차 django 개발 시작

--- mysite/views.py
+++ mysite/views.py
@@ -1,10 +1,13 @@
 # from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import  get_object_or_404, render
 from .models import MainContent
 
 def index(request):
-    # return HttpResponse("Hello world")
-
     content_list = MainContent.objects.order_by('-pub_date')
     context = {'content_list': content_list}
     return render(request, 'mysite/content_list.html', context)
+
+def detail(request, content_id): # 매개변수 content_id 추가 url의 2 의미
+     content_list =  get_object_or_404(MainContent, pk=content_id)
+     context = { 'content_list': content_list}
+     return render(request, 'mysite/content_detail.html', context)
